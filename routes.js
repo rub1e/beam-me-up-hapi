4e12be9498d92784ecba9cf47bd80c9591c531f0
rub1e <alex.rubner@gmail.com>
2015-06-16 08:04:24 +0100

    fixed pix

--- routes.js
+++ routes.js
@@ -12,11 +12,13 @@ module.exports = [
     path: '/upload',
     method: 'POST',
     handler: function (request, reply){
-      fs.readdirSync('pix',function(err,files){
-        if (err) fs.mkdirSync('pix');
+      fs.stat('pix',function(err,stats){
+        if (err) {
+          fs.mkdirSync('pix'
+        )};
+        var piccy = fs.createWriteStream('pix/'+request.payload.title);
+        piccy.write(request.payload.upload);
       });
-      var piccy = fs.createWriteStream('pix/'+request.payload.title);
-      piccy.write(request.payload.upload);
     }
   },
   {
